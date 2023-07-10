d2e499f6beaefc1ee13e69615a188b459d6a4db9
ViKtorXd <victor.suquilanda@udla.edu.ec>
2023-07-10 16:20:52 -0500

    Calculo del promedio

--- prueba.c
+++ prueba.c
@@ -1,19 +1,28 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
-    int id, num;
-    char nom[20], carr[20];
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int id;
+    char nom[50], carr[50];
+    float sum;
     float not1, not2, not3;
-    printf("INgresa un num");
-    scanf("%d",num);
-    FILE *archivo;
+
+    FILE *archivo, *temp;
     archivo = fopen("alumnos.txt", "r");
-    while(!feof(archivo)){
-        fscanf(archivo, "%d;%[^;];%[^;];%f;%f;%f",&id,&nom,&carr,&not1,&not2,&not3);
-        if((num==id)==0){
-            printf("%d %s %s %.2f %.2f %.2f\n",id,nom,carr,not1,not2,not3);
+    temp = fopen("temp.txt", "w");
+
+    if (archivo && temp) {
+        while (fscanf(archivo, "%d;%[^;];%[^;];%f;%f;%f\n", &id, nom, carr, &not1, &not2, &not3) == 6) {
+            sum=0;
+            printf("%d;%s;%s;%.2f;%.2f;%.2f\n", id, nom, carr, not1, not2, not3);
+            sum = ((not1+not2+not3)/3);
+            printf("%.2f\n",sum);
+            fprintf(temp,"%d;%s;%s;%.2f\n", id, nom, carr, sum);
         }
+        fclose(archivo);
+        fclose(temp);
     }
-    fclose(archivo);
     return 0;
 }
+
+
